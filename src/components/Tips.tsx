842379081c840d94a9031c775737b0a8f9790f67
AlexB <alex.bodnar315@gmail.com>
2023-11-27 11:21:31 +0200

    make tips clickable

--- src/components/Tips.tsx
+++ src/components/Tips.tsx
@@ -39,25 +39,33 @@ type Tip = {
 const Tips = () => {
     const [randomTip, setRandomTip] = useState<Tip | null>(null);
 
-    useEffect(() => {
+    const selectRandomTip = () => {
         const randomIndex = Math.floor(Math.random() * tips.length);
         const selectedTip = tips[randomIndex];
         setRandomTip(selectedTip);
-    }, [])
+    };
+
+    useEffect(() => {
+        selectRandomTip();
+    }, []);
+
+    const handleTipClick = () => {
+        selectRandomTip();
+    };
 
     if (!randomTip) {
-        return null; 
+        return null;
     }
-    
+
     return (
         <div className='bg-white w-full pb-4 flex justify-center relative'>
-            <div>
+            <div onClick={handleTipClick} className='cursor-pointer'>
                 <div className='pt-3'>
-                    <LazyLoadImage src={randomTip.image} className='2xl:w-[250px] w-[200px]' effect="blur" PlaceholderSrc="defaults/logoclear.png"/>
+                    <LazyLoadImage src={randomTip.image} className='2xl:w-[250px] w-[200px] hover:drop-shadow-2xl duration-150' effect="blur" PlaceholderSrc="defaults/logoclear.png"/>
                 </div>
                 <div
                     key={randomTip.id}
-                    className="absolute bottom-5 left-0 w-full text-center bg-white text-medium lg:text-base text-sm gilroy flex items-center p-2 px-4 rounded-3xl text-[#919191] shadow-[#ccc] shadow-md"
+                    className="absolute bottom-5 left-0 w-full text-center bg-white text-medium lg:text-base text-sm gilroy flex items-center p-2 px-4 rounded-3xl text-[#919191] shadow-[#ccc] shadow-xl"
                 >
                     {randomTip.title}
                 </div>
